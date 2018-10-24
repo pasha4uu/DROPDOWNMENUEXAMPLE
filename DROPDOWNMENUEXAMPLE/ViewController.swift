afe0c05c0b5d48b96e9b6f0d5bf0d0bd961d9b77
pasha4uu <pasha7864u@gmail.com>
2018-10-25 00:24:33 +0530

    first comment

--- DROPDOWNMENUEXAMPLE/ViewController.swift
+++ DROPDOWNMENUEXAMPLE/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+  @IBOutlet weak var btnClick: UIButton!
+  var arr = [Int]()
+  @IBOutlet weak var myTB: UITableView!
   override func viewDidLoad() {
     super.viewDidLoad()
+    arr = [1,2,3,4,5,6,7,8,9,0]
+    
+    self.myTB.isHidden = true
     // Do any additional setup after loading the view, typically from a nib.
   }
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return arr.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CELL")
 
+      cell?.textLabel?.text = "\(arr[indexPath.row])"
 
+    return cell!
+  }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let cell = tableView.cellForRow(at: indexPath)
+ self.btnClick.setTitle(cell?.textLabel?.text, for: .normal)
+    self.myTB.isHidden = true
+  }
+  
+  @IBAction func btnTap(_ sender: Any) {
+    self.myTB.isHidden = !self.myTB.isHidden
+  }
+  
 }
 
